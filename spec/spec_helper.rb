7de070ef5f911c0c26fb3f2943b2907f6a4e9dbc
in5058 <in5058@in5058.local>
2013-07-18 13:14:51 +0400

    change coveralls 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'hashie'
 require 'coveralls'
 
-Coverails.wear!
+Coveralls.wear!
 
 RSpec.configure do |config|
  #  config.treat_symbols_as_metadata_keys_with_true_values = true
