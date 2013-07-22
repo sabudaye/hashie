9c2fa80726a562f88a5288e0b1d4779693fd8820
cafekoma <cafekoma@gmail.com>
2013-07-22 06:12:53 +0000

    added define_method

--- lib/hashie.rb
+++ lib/hashie.rb
@@ -1,24 +1,36 @@
 require "hashie/version"
 
 module Hashie
-  # Your code goes here...
   class Mash < Hash
-    def initialize
-      
-    end
 
-    def method_missing(full_method, *args, &block)
+    def method_missing(full_method, *args)
       full_method = full_method.to_s
       case full_method[-1]
-        when "=" 
+      when "=" 
+        define_singleton_method(full_method) do |*args|
           self[full_method[0..-2]] = args.first
-        when "?" 
+        end
+        send(full_method, *args)
+      when "?" 
+        define_singleton_method(full_method) do
           self.has_key?(full_method[0..-2])
-        when "!" 
+        end
+        send(full_method)
+      when "!" 
+        define_singleton_method(full_method) do
           self[full_method[0..-2]] = self.class.new
-        else 
+        end
+        send(full_method)
+      else 
+        define_singleton_method(full_method) do
           self.has_key?(full_method) ? self[full_method] : nil
+        end
+        send(full_method)
       end
     end
+  end
+
+  class Dash < Hash
+    
   end
 end
--- spec/mash_spec.rb
+++ spec/mash_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Hashie::Mash do
-  before(:each) do
+  before do
     @mash = Hashie::Mash.new
   end
 
@@ -20,10 +20,20 @@ describe Hashie::Mash do
     @mash.name?.should be_true
   end
 
-  it "True" do
+  it "should create new Mash as key" do
     @mash.name = "Name"
     @mash.name!.add = "add"
     @mash.name.add.should == "add"
   end
 
+  it "should return a Hashie::Mash when passed a bang method to a non-existenct key" do
+   expect(@mash.abc!.is_a?(Hashie::Mash)).to be_true
+  end
+
+  it "should allow for multi-level assignment through bang methods" do
+    @mash.author!.name = "Michael Bleigh"
+    expect(@mash.author).to eq("name" => "Michael Bleigh")
+    @mash.author!.website!.url = "http://www.mbleigh.com/"
+    expect(@mash.author.website).to eq("url" => "http://www.mbleigh.com/")
+  end
 end
